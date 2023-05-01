9ac0e843fd0f6e8c316071c92f7c074d6ca318ac
Barba2k2 <lorenzo.dz@hotmail.com>
2023-05-01 12:34:55 -0300

    Inicio da classe Snake

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ let helpers = {
         return v1.x == v2.x && v1.y == v2.y;
     },
     garbageCollector() {
-        for (let i = 0; i < particles.length; i++){
+        for (let i = 0; i < particles.length; i++) {
             if (particles[i].size <= 0) {
                 particles.splice(i, 1);
             }
@@ -68,6 +68,109 @@ let helpers = {
             CTX.moveTo(0, f);
             CTX.lineTo(W, f);
             CTX.stroke();
+            CTX.closePath();
         }
+    },
+    randHue() {
+        return ~~(Math.random() * 360)
+    },
+    hsl2rgb(hue, saturation, lightness) {
+        if (hue == undefined) {
+            return [0, 0, 0];
+        }
+        let chroma = (1 - Math.abs(2 * lightness - 1)) * saturation;
+        let huePrime = hue / 60;
+        let secondComponent = chroma * (1 - Math.abs((huePrime % 2) - 1));
+
+
+        huePrime = ~~huePrime;
+        let red;
+        let green;
+        let blue;
+
+        if (huePrime === 0) {
+            red = chroma;
+            green = secondComponent;
+            blue = 0;
+        } else if (huePrime === 1) {
+            red = secondComponent;
+            green = chroma;
+            blue = 0;
+        } else if (huePrime === 2) {
+            red = 0;
+            green = chroma;
+            blue = secondComponent;
+        } else if (huePrime === 3) {
+            red = 0;
+            green = secondComponent;
+            blue = chroma;
+        } else if (huePrime === 4) {
+            red = secondComponent;
+            green = 0;
+            blue = chroma;
+        } else if (huePrime === 5) {
+            red = chroma;
+            green = 0;
+            blue = secondComponent;
+        }
+        let lightnessAdjustment = lightness - chroma / 2;
+        red += lightnessAdjustment;
+        green += lightnessAdjustment;
+        blue += lightnessAdjustment;
+
+        return [
+            Math.round(red * 255),
+            Math.round(green * 255),
+            Math.round(blue * 255),
+        ];
+    },
+    lerp(start, end, t) {
+        return start * (1 - t) * end * t;
+    }
+};
+
+// Keys
+
+let KEY = {
+    ArrowUp: false,
+    ArrowRight: false,
+    ArrowDown: false,
+    ArrowLeft: false,
+    resetState() {
+        this.ArrowUp = false;
+        this.ArrowRight = false;
+        this.ArrowDown = false;
+        this.ArrowLeft = false;
+    },
+    listen() {
+        addEventListener(
+            "keydown",
+            (e) => {
+                if (e.key === "ArrowUp" && this.ArrowDown) return;
+                if (e.key === "ArrowDown" && this.ArrowUp) return;
+                if (e.key === "ArrowLeft" && this.ArrowRight) return;
+                if (e.key === "ArrowRight" && this.ArrowLeft) return;
+                this[e.key] = true;
+                Object.keys(this).filter((f) => !== e.key && f !== "listen" && f !== "resetState").forEach((k) => {
+                    this[k] = false;
+                });
+            },
+            false
+        );
+    }
+};
+
+// Classes (Snake, Food, Particle)
+class Snake{
+    constructor(t, type) {
+        this.pos = new helpers.Vec(W / 2, H / 2);
+        this.dir = new helpers.Vec(0, 0);
+        this.type = type;
+        this.index = 1;
+        this.delay = 5;
+        this.size = W / colis;
+        this.color = "white";
+        this.history = [];
+        this.total = 1;
     }
 }
